d62bb896c5eaaa830f27db1516b760579f444dd5
sanhae <tksrha9513@naver.com>
2023-03-29 13:26:16 +0900

    230329

---
+++ 23.03.27 project/23.03.27 project/23.03.27 project.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int sum_of_N(int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n + 1; i++) {
+		sum = sum + i;
+	}
+	return sum;
+}
+
+int sum_of_N2(int n) {
+	return (n * (n + 1)) / 2;
+}
---
+++ sanhae2/sanhae2/sanhae2.cpp
@@ -0,0 +1,16 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+	clock_t start, finish;
+	double duration;
+
+	start = clock();
+	int a = 10 + 22;
+	finish = clock();
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("%f 초가 걸렸습니다.\n", duration);
+	return 0;
+}
