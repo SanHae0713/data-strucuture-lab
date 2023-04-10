b82cf29dfb8d36104e9e0c1a3234cdfd838c976c
sanhae <tksrha9513@naver.com>
2023-04-10 12:12:20 +0900

    4월10일

---
+++ etd/230405 car/230405 car/230405 car.cpp
@@ -0,0 +1,3 @@
+﻿#include{stdio.h}
+
+printf("hello hi");
---
+++ etd/230405 carproject/230405 carproject.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+// Define the SportsCar class
+typedef struct {
+    int top_speed;
+    int acceleration_time;
+    int horsepower;
+} SportsCar;
+
+// Method to print the car's details
+void print_car_details(SportsCar* car) {
+    printf("Top Speed: %d mph\n", car->top_speed);
+    printf("Acceleration Time: %d seconds\n", car->acceleration_time);
+    printf("Horsepower: %d hp\n", car->horsepower);
+}
+
+int main() {
+    // Create a new SportsCar object
+    SportsCar my_car = { 200, 6, 400 };
+
+    // Print the car's details
+    printf("My Car:\n");
+    print_car_details(&my_car);
+
+    return 0;
+}
