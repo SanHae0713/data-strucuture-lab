d01fdec80495fe726d3bb17afb0387b0bf3c3b2b
sanhae <tksrha9513@naver.com>
2023-04-11 10:17:49 +0900

    23월4월10일

--- Array_class_example.cpp
+++ Array_class_example.cpp
@@ -1,9 +1,76 @@
-#include <stdio.h>
+#pragma once
+#include <cstdio>
+#include <vector>	
+#include <iostream>	
+#define MAX_DEGREE 100
+using namespace std;
 
-int main() {
-	int a = 10;
-	int b = 20;
-	printf("%d %d\n",a , b);
+class PolynomialV {
+	vector <float> coef;
+public:
+	int degree() {
+		return coef.size();		
+	}
+	void read() {
+		int deg;
+		float value;
+		printf("다항식의 최고 차수를 입력하세요: ");
+		scanf_s("%d", &deg);		
+		printf("각 항의 계수를 입력하세요 (총 %d개): ", deg + 1);
+		for (int i = 0; i <= deg; i++)
+		{
+			scanf_s("%f", &value);
+			coef.push_back(value);
+		}
+	}
+};
 
-	return 0;
-}
+
+class Polynomial {
+	int degree;				
+	float coef[MAX_DEGREE];
+
+public:
+
+	Polynomial() {
+		degree = 0;			
+		coef[0] = 0.0f;		
+	}
+
+	void read() {
+		printf("다항식의 최고 차수를 입력하세요: ");
+		scanf_s("%d", &degree);		
+		printf("각 항의 계수를 입력하세요 (총 %d개): ", degree + 1);
+		for (int i = 0; i <= degree; i++)
+		{
+			scanf_s("%f", coef + i);
+		}
+	}
+	void display(const char* str = "Poly = ") {
+		printf("\t%s", str);
+		for (int i = 0; i < degree; i++)
+			printf("%5.1f x^%d + ", coef[i], degree - i);
+
+		printf("%4.1f\n", coef[degree]);
+	}
+	void add(Polynomial a, Polynomial b) {
+		if (a.degree > b.degree) {
+			*this = a;		
+			for (int i = 0; i <= b.degree; i++)
+				coef[i + (degree - b.degree)] += b.coef[i];
+		}
+		else {
+			*this = b;		
+			for (int i = 0; i <= a.degree; i++)
+				coef[i + (degree - a.degree)] += a.coef[i];
+		}
+	}
+	bool isZero() {
+		return degree == 0;
+	}
+	void negate() {
+		for (int i = 0; i < degree; i++)
+			coef[i] = -coef[i];
+	}
+
+};
